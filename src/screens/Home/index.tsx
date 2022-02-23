ec3cab166073db3f2cec309efaf2747491515f81
Bruno Lima <francisco.bruno@armateus.com.br>
2022-02-23 00:59:38 -0300

    criando componente Home e tipando png-svg

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from "./styles";
+import LogoSVG from "../../assets/logo.svg";
+
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <LogoSVG width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 Carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,13 +1,28 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.secondary_500};
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  align-items: center;
+  justify-content: space-between;
+
+  flex-direction: row;
+  padding: 32px 24px;
+`;
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
